434860917a5bf485576f070144b2ce87b7e73c8f
Sidi Doueida <s.deoida@interlink.mr>
2025-12-17 13:43:18 +0000

    ERR

--- app/_components/main-nav.jsx
+++ app/_components/main-nav.jsx
@@ -115,7 +115,9 @@ export function MainNav() {
                 </NavigationMenuItem>
               ) : (
                 <NavigationMenuItem key={index}>
-                  <Link href={item.href} legacyBehavior passHref>
+                  <Link href={item.href}>
+                    {/* @next-codemod-error This Link previously used the now removed `legacyBehavior` prop, and has a child that might not be an anchor. The codemod bailed out of lifting the child props to the Link. Check that the child component does not render an anchor, and potentially move the props manually to Link. */
+                    }
                     <NavigationMenuLink
                       className={cn(
                         pathname === item.href && "text-accent-hover",
