d38f737c3004218d4e549be0087935a688f9f941
Sidi Doueida <s.deoida@interlink.mr>
2026-02-12 11:41:58 +0000

    Fixed P/json

--- src/app/_lib/utils.ts
+++ src/app/_lib/utils.ts
@@ -6,8 +6,8 @@ export function cn(...inputs: ClassValue[]) {
 }
 
 // STRAPI URL
-export const STRAPI_URL = "https://cms-interlink.onrender.com";
-//export const STRAPI_URL = "http://localhost:1337";
+// export const STRAPI_URL = "https://cms-interlink.onrender.com";
+export const STRAPI_URL = "http://localhost:1337";
 
 // Google Map URL
 export const mapURL =
